f4e4e99e3558d6c4053caa52b995aca0b455bd46
NickAc <32451103+NickAcPT@users.noreply.github.com>
2021-06-25 09:30:47 +0100

    Add scripts

---
+++ scripts/upstreamCommit.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+(
+set -e
+PS1="$"
+
+function changelog() {
+    base=$(git ls-tree HEAD $1  | cut -d' ' -f3 | cut -f1)
+    cd $1 && git log --oneline ${base}...HEAD
+}
+purpur=$(changelog .gradle/caches/purpurweight/upstreams/Purpur)
+
+updated=""
+logsuffix=""
+if [ ! -z "$purpur" ]; then
+    logsuffix="$logsuffix\n\nPurpur Changes:\n$purpur"
+    updated="Purpur"
+fi
+disclaimer="Upstream has released updates that appear to apply and compile correctly"
+
+if [ ! -z "$1" ]; then
+    disclaimer="$@"
+fi
+
+log="${UP_LOG_PREFIX}Updated Upstream ($updated)\n\n${disclaimer}${logsuffix}"
+
+echo -e "$log" | git commit -F -
+
+) || exit 1
